a634b95880e67f26dfb2f140ebb19be197b4b203
Kenneth Emanuel <ken.emanuel@gmail.com>
2015-06-29 09:59:03 -0700

    Fizzbuzz using object oriented concepts.

---
+++ fizzbuzz.js
@@ -0,0 +1,42 @@
+function FizzBuzzer(numbers) {
+    this.numbers = numbers;
+    return {
+        fizz: function() {
+            for(var i = 0; i < numbers.length; i++) {
+                if(numbers[i]%3 ==0 && numbers[i]%5 == 0) {
+                    console.log(numbers[i]);
+                } else if(numbers[i]%3 == 0) {
+                    console.log('fizz');
+                } else {
+                    console.log(numbers[i]);
+                }
+            }
+        },
+        buzz: function() {
+            for(var i = 0; i < numbers.length; i++) {
+                if(numbers[i]%3 ==0 && numbers[i]%5 == 0) {
+                    console.log(numbers[i]);
+                } else if(numbers[i]%5 == 0) {
+                    console.log('buzz');
+                } else {
+                    console.log(numbers[i]);
+                }
+            }
+        },
+        fizzbuzz: function() {
+            for(var i = 0; i < numbers.length; i++) {
+                if(numbers[i]%3 == 0 && numbers[i]%5 == 0) {
+                    console.log('fizzbuzz');
+                } else {
+                    console.log(numbers[i]);
+                }
+            }
+        }
+    };
+}
+
+var myFizzBuzzer = new FizzBuzzer([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+
+//myFizzBuzzer.fizz();
+myFizzBuzzer.buzz();
+//myFizzBuzzer.fizzbuzz();
